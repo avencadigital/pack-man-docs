cbf5312cc719babbb3c1982bc6d7ba57871b7895
Gustavo Z. Paitch <gzpaitch@live.com>
2025-12-03 13:28:09 -0300

    Feat: Update packages.json

--- .source/server.ts
+++ .source/server.ts
@@ -18,8 +18,8 @@ import * as __fd_glob_8 from "../content/docs/api/analyze-packages.mdx?collectio
 import * as __fd_glob_7 from "../content/docs/index.mdx?collection=docs"
 import * as __fd_glob_6 from "../content/docs/changelog.mdx?collection=docs"
 import { default as __fd_glob_5 } from "../content/docs/guides/meta.json?collection=docs"
-import { default as __fd_glob_4 } from "../content/docs/extensions/meta.json?collection=docs"
-import { default as __fd_glob_3 } from "../content/docs/getting-started/meta.json?collection=docs"
+import { default as __fd_glob_4 } from "../content/docs/getting-started/meta.json?collection=docs"
+import { default as __fd_glob_3 } from "../content/docs/extensions/meta.json?collection=docs"
 import { default as __fd_glob_2 } from "../content/docs/architecture/meta.json?collection=docs"
 import { default as __fd_glob_1 } from "../content/docs/api/meta.json?collection=docs"
 import { default as __fd_glob_0 } from "../content/docs/meta.json?collection=docs"
@@ -31,4 +31,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "api/meta.json": __fd_glob_1, "architecture/meta.json": __fd_glob_2, "getting-started/meta.json": __fd_glob_3, "extensions/meta.json": __fd_glob_4, "guides/meta.json": __fd_glob_5, }, {"changelog.mdx": __fd_glob_6, "index.mdx": __fd_glob_7, "api/analyze-packages.mdx": __fd_glob_8, "api/health.mdx": __fd_glob_9, "api/index.mdx": __fd_glob_10, "architecture/overview.mdx": __fd_glob_11, "extensions/chrome.mdx": __fd_glob_12, "extensions/overview.mdx": __fd_glob_13, "extensions/vscode.mdx": __fd_glob_14, "getting-started/github-token.mdx": __fd_glob_15, "getting-started/introduction.mdx": __fd_glob_16, "getting-started/quick-start.mdx": __fd_glob_17, "getting-started/supported-formats.mdx": __fd_glob_18, "guides/analyzing-dependencies.mdx": __fd_glob_19, "guides/github-integration.mdx": __fd_glob_20, "guides/troubleshooting.mdx": __fd_glob_21, "guides/understanding-results.mdx": __fd_glob_22, "guides/updating-packages.mdx": __fd_glob_23, });
+export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "api/meta.json": __fd_glob_1, "architecture/meta.json": __fd_glob_2, "extensions/meta.json": __fd_glob_3, "getting-started/meta.json": __fd_glob_4, "guides/meta.json": __fd_glob_5, }, {"changelog.mdx": __fd_glob_6, "index.mdx": __fd_glob_7, "api/analyze-packages.mdx": __fd_glob_8, "api/health.mdx": __fd_glob_9, "api/index.mdx": __fd_glob_10, "architecture/overview.mdx": __fd_glob_11, "extensions/chrome.mdx": __fd_glob_12, "extensions/overview.mdx": __fd_glob_13, "extensions/vscode.mdx": __fd_glob_14, "getting-started/github-token.mdx": __fd_glob_15, "getting-started/introduction.mdx": __fd_glob_16, "getting-started/quick-start.mdx": __fd_glob_17, "getting-started/supported-formats.mdx": __fd_glob_18, "guides/analyzing-dependencies.mdx": __fd_glob_19, "guides/github-integration.mdx": __fd_glob_20, "guides/troubleshooting.mdx": __fd_glob_21, "guides/understanding-results.mdx": __fd_glob_22, "guides/updating-packages.mdx": __fd_glob_23, });
